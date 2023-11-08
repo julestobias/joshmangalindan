d74ef456e585c35645cfe48b3b391d27f1317791
CodeSandbox <support@codesandbox.io>
2023-11-08 08:56:52 +0000

    Initial commit

---
+++ src/components/Collage.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+import React from "react";
+
+const Collage = () => {
+  return (
+    <div className={`flex justify-center h-full`}>
+      <div className={`bg-white m-5 w-5/6 h-full`}>
+        <p>TEST</p>
+      </div>
+    </div>
+  );
+};
+
+export default Collage;
